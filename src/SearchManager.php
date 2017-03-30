803a80cfcd6db73377a2347da89ebc7f29289235
Johan Gaub <johangaub@gmail.com>
2017-03-30 16:45:07 +0200

    SearchManager class added

--- src/SearchManager.php
+++ src/SearchManager.php
@@ -1,12 +1,89 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: wilder
- * Date: 30/03/17
- * Time: 11:54
- */
-class SearchManager
+
+namespace omdb;
+
+class searchManager
 {
 
+    private $api = 'http://www.omdbapi.com/?';
+    private $title;
+    private $type;
+    private $year;
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param mixed $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    /**
+     * @param mixed $year
+     */
+    public function setYear($year)
+    {
+        $this->year = $year;
+    }
+
+    public function getList()
+    {
+        $url = $this->api . "s=" . $this->title . "&type=" . $this->type . "&y=" . $this->year;
+        $ch = curl_init();
+// Disable SSL verification
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+// Will return the response, if false it print the response
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// Set the url
+        curl_setopt($ch, CURLOPT_URL,$url);
+// Execute
+        $result=curl_exec($ch);
+// Closing
+        curl_close($ch);
+
+        return json_decode($result);
+    }
 }
+
+
+
+
+
+
+
+
+
+
